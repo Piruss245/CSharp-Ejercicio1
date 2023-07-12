ad54f0e4543fcbb25dcb869d676e2515690e08ba
Piruss245 <138598423+Piruss245@users.noreply.github.com>
2023-07-12 12:13:36 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+// EJERCICIO 1 - Escribe un programa que reciba tu nombre y lo escriba por consola.
+Console.WriteLine("Escribe tu nombre");
+string nombre = Console.ReadLine();
+Console.WriteLine("Hola, " + nombre);
+
+
+
+// EJERCICIO 2 - Escribe un programa que tome la hora y la escriba por pantalla.
+Console.WriteLine("¿Qué Hora es?");
+string hora = Console.ReadLine();
+Console.WriteLine("La hora es: " + hora);
